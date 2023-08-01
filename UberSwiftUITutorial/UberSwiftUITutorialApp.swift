31d5d5742611094ca6b7026293178c2af102a6bf
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-08-01 21:47:50 +0300

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Sasha Maksyutenko on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
